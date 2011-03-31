69c99967379ab5f69c039a8e90fed1d6745ffb3a
Sagie Maoz <sagie@maoz.info>
2011-03-31 23:01:18 +0200

    adding fools.php

---
+++ fools.php
@@ -0,0 +1,394 @@
+<?php
+$T['common']['types'][1] = 'אושיה';
+$T['common']['types'][2] = 'אגודה';
+$T['common']['types'][3] = 'יומן';
+$T['common']['types'][4] = 'מענה';
+$T['common']['types'][5] = 'מענה';
+$T['common']['types'][7] = 'רב-שיח';
+$T['common']['types'][8] = 'מליאה';
+$T['common']['types'][9] = 'כתב-יד';
+$T['common']['types'][10] = 'מיצג';
+$T['common']['types'][11] = 'צלמית';
+$T['common']['types'][32] = 'משאל';
+$T['common']['types'][24] = 'קובץ שמע';
+$T['common']['types'][25] = 'קובץ אור-קולי';
+
+$T['common']['multi_types'][1] = 'אושיות';
+$T['common']['multi_types'][2] = 'אגודות';
+$T['common']['multi_types'][3] = 'יומנים';
+$T['common']['multi_types'][4] = 'מענים';
+$T['common']['multi_types'][5] = 'מענים';
+$T['common']['multi_types'][7] = 'רבי-שיח';
+$T['common']['multi_types'][8] = 'מליאות';
+$T['common']['multi_types'][9] = 'כתבים';
+$T['common']['multi_types'][10] = 'מיצגים';
+$T['common']['multi_types'][11] = 'צלמיות';
+$T['common']['multi_types'][32] = 'משאלים';
+$T['common']['multi_types'][24] = 'קבצי שמע';
+$T['common']['multi_types'][25] = 'קבצים אור-קוליים';
+
+$T['common']['navbar'] = array(
+	'main'		 => 'ראשי',
+	'me'		 => 'אנוכי',
+	'communities'=> 'אגודות',
+	'mycommunities'=> 'אגודותיי',
+	'users'		 => 'אושיות',
+	'myusers'	 => 'רעיי',
+	'blogs'		 => 'כתבי-יד',
+	'forums'	 => 'רבי-שיח',
+	'images'	 => 'צלמיות',
+	'music'		 => 'קבצי שמע',
+	'video'		 => 'קבצים אור-קוליים',
+	'dex2go'	 => 'דקס לדרך',
+	'myhome'	 => 'ביתי',
+	'logout'	 => 'פרידה',
+	'login'		 => 'חבירה',
+	'join'		 => 'הצטרפות',
+	'about'		 => 'אודות',
+	'help'		 => 'עזרה',
+	'contact'	 => 'צור קשר',
+	'copyright'	 => '© זכויות יוצרים'
+);
+
+$T['common']['relation_log'] = array(
+	1	=> 'אורחיי',
+	2	=> 'מארחיי',
+	3	=> 'כתבו לי',
+	4	=> 'כתבתי להם',
+	5	=> 'דיברו איתי',
+	6	=> 'דיברתי איתם',
+);
+
+
+$T['common']['secnavbar'] = array(
+	'homepage'		=> 'העמוד שלי',
+	'friends'		=> 'רעיי',
+
+	'allblog'		=> 'כל היומנים',
+	'blog'			=> 'יומנים',
+	'blogbest'		=> 'הגזעיים',
+	'blognew'		=> 'החדשים',
+	'blogpopular'	=> 'הפופולריים',
+	'blogfriends'	=> 'של רעים',
+	'account'		=> 'עריכת חשבון',
+	'postreplied'	=> 'כתבים שהגבתי להם',
+	'postnew'		=> 'כתבים חדשים',
+
+	'reviewsbest'	=> 'הגזעיות',
+	'reviewspopular'=> 'הפופולריות',
+	'reviewsnew'	=> 'החדשות',
+	'reviewsfriends'=> 'של חברים',
+	'reviewsreplied'=> 'שהגבתי עליהן',
+
+	'nowpeople'		=> 'עכשיו באתר',
+	'allpeople'		=> 'כל האושיות',
+	'peoplebest'	=> 'הגזעיות',
+	'peoplenew'		=> 'החדשות',
+	'peoplepopular'	=> 'הפופולריות',
+	'connected'		=> 'בקשר איתי',
+
+	'allcommunities'	=> 'כל האגודות',
+	'communities'		=> 'אגודות',
+	'communitiesmanage'	=> 'אגודות בראשותי',
+	'communitiespopular'=> 'הגזעיות',
+	'communitiesbest'	=> 'המעניינות',
+	'communitiesnew'	=> 'החדשות',
+	'communitiesfriends'=> 'אגודות של רעים',
+
+	'allimages'		=> 'כל הצלמיות',
+	'images'		=> 'הצלמיות שלי',
+	'imagesnew'		=> 'החדשות',
+	'imagesbest'	=> 'הגזעיות',
+	'imagespopular'	=> 'הפופולריות',
+	'imagesfriends'	=> 'צלמיות של רעים',
+
+	'allvideo'		=> 'כל הקבצים האור-קוליים',
+	'video'			=> 'האור-קוליים שלי',
+	'videonew'		=> 'החדשים',
+	'videobest'		=> 'הגזעיים',
+	'videopopular'	=> 'הפופולריים',
+	'videofriends'	=> 'אור-קוליים של רעים',
+
+	'allmusic'		=> 'כל קבצי השמע',
+	'music'			=> 'קבצי השמע שלי',
+	'musicnew'		=> 'החדשים',
+	'musicbest'		=> 'הגזעיים',
+	'musicpopular'	=> 'הפופולארים',
+	'musicfriends'	=> 'קבצי שמע של רעים',
+
+	'gallerynew'	=> 'מיצגים חדשים',
+	'gallerybest'	=> 'מיצגים גזעיים',
+	'allgallery'	=> 'כל המיצגים',
+
+	'alltopics'		=> 'כל רבי-השיח',
+	'topics'		=> 'רבי-השיח שלי',
+	'topicsbest'	=> 'הגזעיים',
+	'topicsnew'		=> 'החדשים',
+	'topicspopular'	=> 'הפופולריים',
+	'topicsfriends'	=> 'רבי-השיח של רעיי',
+	'topicsmy'	=> 'רבי-השיח שלי',
+
+	'forumbest'		=> 'המליאות הטובות',
+	'forumnew'		=> 'המליאות החדשות',
+
+	'galleries'		=> 'המיצגים שלי',
+	'mail'			=> 'מכתבייה',
+	'inbox'			=> 'מברקים נכנסים',
+	'outbox'		=> 'מברקים ששוגרו',
+	'compose'		=> 'שלח מברק',
+	'friendsoffriends' => 'רעים של רעים',
+	'pals'			=> 'רעים לאישורי',
+	'blocked'		=> 'חסומים',
+	'edit_relation' => 'עריכת קשר',
+	'disconnect'	=> 'התנתקות מהקהילה',
+	'viewme'		=> $T['common']['relation_log'][1],
+	'writeme'		=> $T['common']['relation_log'][3],
+	'speakme'		=> $T['common']['relation_log'][5],
+	'view'			=> $T['common']['relation_log'][2],
+	'write'			=> $T['common']['relation_log'][4],
+	'speak'			=> $T['common']['relation_log'][6],
+	'mystatus'		=> 'המצב שלי:',
+	'edit'			=> 'עריכת פרופיל',
+	'permissions'	=> 'עריכת גושפנקאות',
+	'design'		=> 'עיצוב העמוד',
+	'addusers'		=> 'בקשות הרעות שלי',
+	'manage_g'		=> 'ניהול',
+	'restore'		=> 'איפוס העיצוב',
+	'email'			=> 'העדפות דואר',
+	'addcontent'	=> 'הוספת תכנים',
+	'editaccount'	=> 'עריכת חשבון'
+);
+
+$T['login']['headline']	= 'חבירה';
+$T['pager']['pages'] = 'גליונות';
+
+$T['common']['design_boxes'] = array(
+	'add'		=> 'החזרת מודולים',
+	'move'		=> 'הזזת מודול',
+	'del'		=> 'הסרת מודול',
+	'save'		=> 'שמירת עיצוב',
+	'cancel'	=> 'ביטול',
+	'style'		=> 'סגנון',
+	'layout'	=> 'עימוד תכנים',
+	1  => 'מברקים נכנסים',
+	2  => 'המניפסט',
+	3  => 'יומן',
+	4  => 'התכנים שיצרתי',
+	5  => 'צלמיות',
+	6  => 'רעים',
+	7  => 'סטטיסטיקה',
+	9  => 'תיבת תוכן',
+	10 => 'קבצים אור-קוליים',
+	11 => 'כתבות',
+	12 => 'אגודות',
+	13 => 'מליאות',
+	14 => 'פרטים',
+	15 => 'תיאור ראשי ותמונה',
+	16 => 'סוגות',
+	17 => 'ארכיון',
+	18 => 'פרופיל',
+	19 => 'מנהלי הקהילה',
+	20 => 'קבצי שמע',
+	21 => 'הרשמה לסינדיקציה',
+	22 => 'תכנים להם הגבתי',
+	23 => 'לאחרונה',
+	24 => 'שמור במועדפים',
+	25 => 'המשפחה שלי',
+	26 => 'הקשרים העסקיים שלי',
+	27 => 'הרעים הטובים שלי',
+	31 => 'הביקורות שלי',
+	32 => 'הפיד שלי',
+);
+
+$T['login']['reg_users'] = 'חבירה:';
+
+$T['search']['headline'][1] = array(
+	1 => 'האושיות הכי מדורגות',
+	2 => 'אושיות חדשות',
+	3 => 'אושיות נצפות',
+	4 => 'אושיות מסקרנות',
+	6 => 'אורחים אחרונים',
+	'your' 		=> 'רעיי',
+	'friends'	=> 'רעיי',
+	'awaiting'	=> 'רעים לאישורי',
+);
+$T['search']['small_headline'][1] = $T['search']['headline'][1];
+$T['search']['headline'][2] = array(
+	2 => 'אגודות חדשות',
+	3 => 'אגודות נצפות',
+	4 => 'אגודות מסקרנות',
+	8 => 'אגודות אחרונות',
+	'your'		=> 'אגודותיי',
+	'friends'	=> 'אגודות של רעים',
+);
+$T['search']['small_headline'][2] = $T['search']['headline'][2];
+$T['search']['headline'][3] = array(
+	2 => 'יומנים חדשים',
+	3 => 'יומנים נקראים',
+	4 => 'יומנים מומלצים',
+	8 => 'יומנים מעודכנים',
+	'your'		=> 'יומניי',
+	'friends'	=> 'יומני רעיי',
+);
+$T['search']['small_headline'][3] = $T['search']['headline'][3];
+$T['search']['headline'][7] = array(
+	2 => 'רבי-שיח חדשים',
+	3 => 'רבי-שיח נצפים',
+	4 => 'רבי-שיח מומלצים',
+	8 => 'רבי-שיח מעודכנים',
+	'your'		=> 'רבי-שיח שלך',
+	'friends'	=> 'רבי-שיח של רעים',
+);
+$T['search']['small_headline'][7] = $T['search']['headline'][7];
+$T['search']['headline'][8] = array(
+	2 => 'מליאות חדשות',
+	3 => 'מליאות נצפות',
+	4 => 'מליאות מומלצות',
+	8 => 'מליאות מעודכנות',
+	'your'		=> 'מליאותיך',
+	'friends'	=> 'מליאות רעיך',
+);
+$T['search']['small_headline'][8] = $T['search']['headline'][8];
+$T['search']['headline'][9] = array(
+	2 => 'כתבים חדשים',
+	3 => 'כתבים נצפים',
+	4 => 'כתבים מומלצים',
+	8 => 'כתבים מעודכנים',
+	'your'		=> 'כתביך',
+	'friends'	=> 'כתבי רעיך',
+);
+$T['search']['small_headline'][9] = $T['search']['headline'][9];
+$T['search']['headline'][10] = array(
+	2 => 'מיצגים חדשים',
+	3 => 'מיצגים נצפים',
+	4 => 'מיצגים מומלצים',
+	6 => 'מיצגים מעודכנים',
+	'your'		=> 'המיצגים שלך',
+	'friends'	=> 'מיצגים של רעים',
+);
+$T['search']['small_headline'][10] = $T['search']['headline'][10];
+$T['search']['headline'][11] = array(
+	2 => 'צלמיות חדשות',
+	3 => 'צלמיות נצפות',
+	4 => 'צלמיות מומלצות',
+	6 => 'צלמיות מעודכנות',
+	'your'		=> 'צלמיותך',
+	'friends'	=> 'צלמיות של רעים',
+);
+$T['search']['small_headline'][11] = $T['search']['headline'][11];
+$T['search']['headline'][24] = array(
+	2 => 'קבצי שמע חדשים',
+	3 => 'קבצי שמע נצפים',
+	4 => 'קבצי שמע מומלצים',
+	6 => 'קבצי שמע מעודכנים',
+	'your'		=> 'קבצי השמע שלך',
+	'friends'	=> 'קבצי שמע של רעים',
+);
+$T['search']['small_headline'][24] = $T['search']['headline'][24];
+$T['search']['headline'][25] = array(
+	2 => 'קבצים אור-קוליים חדשים',
+	3 => 'קבצים אור-קוליים נצפים',
+	4 => 'קבצים אור-קוליים מומלצים',
+	6 => 'קבצים אור-קוליים מעודכנים',
+	'your'		=> 'קבצים אור-קוליים שלך',
+	'friends'	=> 'קבצים אור-קוליים של רעים',
+);
+$T['search']['small_headline'][25] = $T['search']['headline'][25];
+$T['search']['headline'][30] = array(
+	2 => 'משאלים חדשים',
+	3 => 'משאלים נצפים',
+	4 => 'משאלים מומלצים',
+	6 => 'משאלים מעודכנים',
+	'your'		=> 'משאלייך',
+	'friends'	=> 'משאלים של רעים',
+);
+$T['search']['small_headline'][30] = $T['search']['headline'][30];
+$T['search']['headline'][32] = $T['search']['headline'][30];
+$T['search']['small_headline'][32] = $T['search']['headline'][32];
+
+$T['mail']['send']='שגר';
+$T['mail']['mail']='מברק';
+$T['mail']['inbox']='מברקים נכנסים';
+$T['mail']['compose']='מברק חדש';
+$T['mail']['from']='מוען';
+$T['mail']['to']='נמען';
+$T['mail']['subject']='נושא:';
+$T['mail']['reply']='השבה';
+$T['mail']['reply_all']='השבה לכולם';
+$T['mail']['delete']='מחק מברק';
+$T['mail']['block']='חסום אושיה';
+$T['mail']['outbox']='מברקים ששוגרו';
+$T['mail']['message']='מברק';
+$T['mail']['body']='תוכן המברק';
+
+$T['mail']['msg'][1]='המברק שוגר בהצלחה!';
+$T['mail']['msg'][2]='חלה תקלה בשיגור המברק';
+$T['mail']['msg'][3]='מברקים נמחקו';
+
+
+$T['rate']['no stars']=				'error:אין לך מנדטים';
+$T['rate']['not_allowed']=			'error:אין לך גושפנקא לדרג כאן';
+$T['rate']['new user']=				'error:לא ניתן לדרג אושיות חדשות';
+$T['rate']['user_not_active']=		'error:האושיה אינה פעילה ולכן לא ניתן לדרג';
+$T['rate']['vote2fast']=			'error:לא ניתן לדרג את אותה אושיה פעמיים ב24 שעות';
+$T['rate']['rate_error']=			'רק אושיות מדרגה "what" יכולות לראות את הדירוגים של התוכן הזה';
+
+$T['add']['submit']['lable']='עדכן גושפנקאות';
+$T['add']['submit']['more']='עוד גושפנקאות לעריכה:';
+$T['permissions']['permissions']='גושפנקאות';
+$T['permissions']['headline']='ערוך גושפנקאות';
+
+$T['permissions']['manage']='גושפנקאות ניהול';
+$T['permissions']['view']='גושפנקאות צפייה';
+$T['permissions']['allperm']='הצגת גושפנקאות מתקדמות';
+
+$T['add']['navbar']['permissions'] = 'גושפנקאות';
+
+
+$T['add']['submit']['lable']='שיגור';
+
+$T['add']['my_tags']='סוגות רלוונטיות: ';
+
+for($i=0;$i<35;$i++) {
+	$T['type_'.$i]['cat']['input_lable'] = 'סוגות';
+}
+
+$T['common']['back_lobby'] = 'חזרה למבואה';
+$T['common']['lobby'] = 'המבואה';
+$T['type_1']['buisness']['input_lable']='משלח ידי';
+$T['type_1']['cat_company']['input_lable']='הגילדה בה אני חבר';
+$T['type_1']['buisness_profile']="הפרופיל העסקי שלי";
+$T['common']['welcome']='ברכות';
+$T['common']['guest']='פלוני';
+$T['common']['todotmc']=array(
+	'headline'	=>'במה תחפצו?',
+	'homepage'	=>'בדקו את עמודכם',
+	'edit'		=>'נהלו את עמודכם',
+	'visited'	=>'אושפיזין בעמודכם',
+	'connect'	=>'הכירו רעים',
+	'blog'		=>'כתבו יומן',
+	'myblog'	=>'קראו ביומנכם',
+	'com'		=>'הצטרפו לאגודות',
+	'register'	=>'לחתימה',
+	'login'		=>'חבירה',
+	'hello_new'	=>'ברכות פלוני,'
+);
+
+$T['type_1']['view_headline']='משכני';
+$T['type_1']['location']='דר ב';
+$T['type_1']['myprofile']='עמודי';
+$T['type_1']['myfriends']='רעיי';
+$T['type_1']['mypictures']='אמנותי';
+$T['type_1']['blog']='יומני';
+$T['type_1']['my_topics']='יצירותיי';
+$T['type_1']['age']='ותק';
+$T['type_1']['martial_status']['input_lable']='מצב משפחתי';
+$T['type_1']['view_occupation']='משלח יד';
+$T['type_1']['view_want_to_meet']='תר אחר';
+$T['type_1']['sex']['input_lable']='מגדר';
+$T['common']['friends'] = 'רעים';
+$T['common']['stars']= 'מנדטים';
+$T['common']['lost_pw']='האם שכחת צופנך?';
+$T['common']['newmsgs']='מברקים';
+
+?>
